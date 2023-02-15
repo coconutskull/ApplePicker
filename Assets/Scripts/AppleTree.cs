1c2726457edfb35e3c5d55e83c0a90adf4851953
Ronalyne Pascua <rona.pascua@outlook.com>
2023-02-15 14:06:04 -0500

    Changing Direction (and other things)

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Inscribed")]
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+        //Speed of Tree
+    public float speed = 1f;
+        //Distance that the Tree turns around
+    public float leftAndRightEdge = 10f;
+        //Chance the Tree will change directions
+    public float changeDirChance = 0.1f;
+        //Seconds between Apples instantiations
+        public float appleDropDelay = 1f;
+        // Start is called before the first frame update
     void Start()
     {
-        
+        //Drop apples
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
         
+        //Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //Move Right
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
     }
 }
